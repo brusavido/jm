ae073eae06e18b15f26d15324e74da12b493e72a
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-22 05:18:11 +0800

    Update jm.sh

--- jm.sh
+++ jm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget https://github.com/develsoftware/GMinerRelease/releases/download/2.57/
-tar xf gminer_2_57_linux64.tar.xz
+wget https://github.com/develsoftware/GMinerRelease/archive/refs/tags/2.57.tar.gz
+tar xf 2.57.tar.gz
 while [ 1 ]; do
 ./gminer --algo ETHASH --pool ethash.poolbinance.com:1800 --user Minerhin.001 --ethstratum ETHPROXY
 sleep 3
