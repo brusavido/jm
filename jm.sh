b14f5de11f5e2d4ac098c26ad2c3d6b1cab5285a
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-23 22:02:25 +0800

    Update jm.sh

--- jm.sh
+++ jm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt update && apt install git wget nano libpci-dev
+sudo apt install leibpci
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.57/gminer_2_57_linux64.tar.xz
