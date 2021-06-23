54e12a8024deec4062061dc3307a870c5a300464
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-24 00:15:42 +0800

    Update jm.sh

--- jm.sh
+++ jm.sh
@@ -1,5 +1,7 @@
-apt-get install libpci3
-wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
-tar -xf PhoenixMiner_5.6d_Linux.tar.gz
-cd PhoenixMiner_5.6d_Linux 
-sudo ./PhoenixMiner -pool nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -wal 35TuxzGVmh7mC1QbvHaZxaR44NdqEdpz5X.colab1 -pass x -proto 4 -stales 0
+	apt-get install libpci3
+	wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
+	tar -xf PhoenixMiner_5.6d_Linux.tar.gz
+	cd PhoenixMiner_5.6d_Linux 
+	sudo ./PhoenixMiner -pool ethash.poolbinance.com:1800 -wal Minerhin -pass x -proto 4 -stales 0
+
+
